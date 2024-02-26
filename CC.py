94b6313689ab77c53f3d6e187930e773456d56ae
Mattia Genesi <37869247+ghennara@users.noreply.github.com>
2024-02-26 22:21:07 +0100

    Update CC.py

--- CC.py
+++ CC.py
@@ -1,37 +1,32 @@
-def CesarCypher(string, increment):
-    alphabet = "1234567890qwertyuiop[]asdfghjkl;zxcvbnm,.!@#$%^&*()_+-=-{}:<>|QWERTYUIOPASDFGHJKLZXCVBNM ~`?°"
+def CaesarCipher(string, increment):
+    alphabet = "1234567890qwertyuiop[]asdfghjkl;zxcvbnm,.!@#$%^&*()_+-=-{}:<>|QWERTYUIOPASDFGHJKLZXCVBNM ~`?Â°"
     encrypt = ''
-    index=0
- 
-    if increment<1 or increment>50:
-        print('ENCRYPTION FAILED. Put a number in the (1-50) range, you idiot.')
-    else:               
-        for i in range(len(string)):
-            for j in range(len(alphabet)):
-                if string[i] == alphabet[j]:
-                    if (j + increment)>(len(alphabet)-1):
-                        index = j + increment - len(alphabet)
-                        encrypt = encrypt + str(alphabet[index])
-                    else:
-                        encrypt = encrypt + str(alphabet[j+increment])
 
-        return encrypt
+    if increment < 1 or increment > 50:
+        print('ENCRYPTION FAILED. Put a number in the (1-50) range.')
+    else:
+        for char in string:
+            if char in alphabet:
+                index = (alphabet.index(char) + increment) % len(alphabet)
+                encrypt += alphabet[index]
+            else:
+                encrypt += char
 
+    return encrypt
 
-def hackCesar(string, increment):
-    alphabet = "1234567890qwertyuiop[]asdfghjkl;zxcvbnm,.!@#$%^&*()_+-=-{}:<>|QWERTYUIOPASDFGHJKLZXCVBNM ~`?°"
-    encrypt = ''
- 
-    if increment<1 or increment>50:
-        print('DECRYPTION FAILED. It had to be a number in the (1-50) range, you idiot.')
-    else:               
-        for i in range(len(string)):
-            for j in range(len(alphabet)):
-                if string[i] == alphabet[j]:
-                    if (j - increment) < 0:
-                        index = j + len(alphabet) - increment
-                        encrypt = encrypt + str(alphabet[index])
-                    else:
-                        encrypt = encrypt + str(alphabet[j-increment])
+def hackCaesar(string, increment):
+    alphabet = "1234567890qwertyuiop[]asdfghjkl;zxcvbnm,.!@#$%^&*()_+-=-{}:<>|QWERTYUIOPASDFGHJKLZXCVBNM ~`?Â°"
+    decrypt = ''
 
-        return encrypt
+    if increment < 1 or increment > 50:
+        print('DECRYPTION FAILED. Put a number in the (1-50) range.')
+    else:
+        for char in string:
+            if char in alphabet:
+                index = (alphabet.index(char) - increment) % len(alphabet)
+                decrypt += alphabet[index]
+            else:
+                decrypt += char
+
+    return decrypt
+    
